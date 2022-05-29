d203d00204bce52dff7dfcd0c790c548610ab7f2
Blake Wang <blakexwang@gmail.com>
2022-05-29 02:04:28 -0400

    updated navbar menu

--- src/components/StyledNav.js
+++ src/components/StyledNav.js
@@ -7,31 +7,37 @@ export const StyledNav = () => {
     <nav className="navbar navbar-expand-md navbar-dark bg-primary">
       <div className="container">
         <Link to='/' className='navbar-brand'>论文展示项目</Link>
-        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+        <button className="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar">
           <span className="navbar-toggler-icon"></span>
         </button>
-        <div className="collapse navbar-collapse" id="navbarCollapse">
-          <ul className="navbar-nav me-auto mb-2 mb-md-0">
-            <li className='nav-item'>
-              <NavLink to='/' className={({ isActive }) => 
-                isActive ? 'nav-link active' : 'nav-link'
-                }>主页</NavLink>
-            </li>
-            <li className='nav-item'>
-              <NavLink to='about' className={({ isActive }) => 
-                isActive ? 'nav-link active' : 'nav-link'
-                }>活动详情</NavLink>
-            </li>
-            <li className='nav-item'>
-              <NavLink to='essay' className={({ isActive }) => 
-                isActive ? 'nav-link active' : 'nav-link'
-                }>论文列表</NavLink>
-            </li>
-          </ul>
-          <form className="d-flex" role="search">
-            <input className="form-control me-2" type="search" placeholder="搜索标题或者作者" aria-label="Search" />
-            <button className="btn btn-primary" type="submit"><i className="bi bi-search"></i></button>
-          </form>
+        <div class="offcanvas offcanvas-end text-white bg-primary" tabindex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
+          <div class="offcanvas-header">
+            <h5 class="offcanvas-title" id="offcanvasNavbarLabel">Offcanvas</h5>
+            <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+          </div>
+          <div class="offcanvas-body">
+            <ul className="navbar-nav me-auto mb-2 mb-md-0 text-white">
+              <li className='nav-item'>
+                <NavLink to='/' className={({ isActive }) => 
+                  isActive ? 'nav-link active' : 'nav-link'
+                  }>主页</NavLink>
+              </li>
+              <li className='nav-item'>
+                <NavLink to='about' className={({ isActive }) => 
+                  isActive ? 'nav-link active' : 'nav-link'
+                  }>活动详情</NavLink>
+              </li>
+              <li className='nav-item'>
+                <NavLink to='essay' className={({ isActive }) => 
+                  isActive ? 'nav-link active' : 'nav-link'
+                  }>论文列表</NavLink>
+              </li>
+            </ul>
+            <form className="d-flex" role="search">
+              <input className="form-control me-2" type="search" placeholder="搜索标题或者作者" aria-label="Search" />
+              <button className="btn btn-primary" type="submit"><i className="bi bi-search"></i></button>
+            </form>
+          </div>
         </div>
       </div>
     </nav>
