9faef1ef237bce4f6e27ca9beed367036ef99e08
Blake Wang <blakexwang@gmail.com>
2022-05-29 20:54:28 -0400

    updated offcanvas

--- src/components/StyledNav.js
+++ src/components/StyledNav.js
@@ -1,7 +1,12 @@
 import React from 'react'
-import { Link, NavLink } from 'react-router-dom'
+import { Link, NavLink, useNavigate } from 'react-router-dom'
 
 export const StyledNav = () => {
+  const nevigate = useNavigate()
+
+  const handleNav = (target) => {
+    nevigate(target)
+  }
 
   return (
     <nav className="navbar navbar-expand-md navbar-dark bg-primary">
@@ -12,25 +17,34 @@ export const StyledNav = () => {
         </button>
         <div class="offcanvas offcanvas-end text-white bg-primary" tabindex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
           <div class="offcanvas-header">
-            <h5 class="offcanvas-title" id="offcanvasNavbarLabel">Offcanvas</h5>
-            <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+            <h5 class="offcanvas-title" id="offcanvasNavbarLabel">论文展示项目</h5>
+            <button type="button" class="btn-close btn-close-white" data-bs-dismiss="offcanvas" aria-label="Close"></button>
           </div>
           <div class="offcanvas-body">
             <ul className="navbar-nav me-auto mb-2 mb-md-0 text-white">
               <li className='nav-item'>
                 <NavLink to='/' className={({ isActive }) => 
                   isActive ? 'nav-link active' : 'nav-link'
-                  }>主页</NavLink>
+                  }
+                  data-bs-dismiss="offcanvas"
+                  onClick={() => handleNav('/')}
+                  >主页</NavLink>
               </li>
               <li className='nav-item'>
                 <NavLink to='about' className={({ isActive }) => 
                   isActive ? 'nav-link active' : 'nav-link'
-                  }>活动详情</NavLink>
+                  }
+                  data-bs-dismiss="offcanvas"
+                  onClick={() => handleNav('about')}
+                  >活动详情</NavLink>
               </li>
               <li className='nav-item'>
                 <NavLink to='essay' className={({ isActive }) => 
                   isActive ? 'nav-link active' : 'nav-link'
-                  }>论文列表</NavLink>
+                  }
+                  data-bs-dismiss="offcanvas"
+                  onClick={() => handleNav('essay')}
+                  >论文列表</NavLink>
               </li>
             </ul>
             <form className="d-flex" role="search">
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -14,7 +14,7 @@ const Home = () => {
       <div class="container col-xxl-8 px-4 py-5">
         <div class="row flex-lg-row align-items-center g-5 py-5 mt-2">
           <div class="col-lg-6">
-            <h1 class="display-5 fw-bold lh-1 mb-3">2022 北京大学夏季 event</h1>
+            <h1 class="display-5 fw-bold lh-1 mb-3">2022 XX大学 夏季 event</h1>
             <p class="lead">活动简介</p>
             <div class="justify-content-md-start">
               <Link to='essay' className='btn btn-primary btn-lg px-4 me-md-2'>浏览论文</Link>
